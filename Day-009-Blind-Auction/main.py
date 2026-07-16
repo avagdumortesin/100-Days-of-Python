a8c88b1a4823afdf3c0df0c49c2a70b2112c3ba5
Avagdu Mortesin <55938242+avagdumortesin@users.noreply.github.com>
2026-07-16 03:14:39 -0700

    Add Day 16, plus instructions for additional days

--- Day-009-Blind-Auction/main.py
+++ Day-009-Blind-Auction/main.py
@@ -1,3 +1,26 @@
+"""
+Instructions:
+The goal is to build a blind auction program.
+
+Clearing the Output
+There are several ways of clearing the output. The easiest is to simply print "\n" many times so that the output scrolls down many lines.
+
+e.g.
+
+# This will add 20 new lines to the output
+print("\n" * 20)
+
+Functionality
+- Each person writes their name and bid.
+- The program asks if there are others who need to bid. If so, then the computer clears the output (prints several blank lines) then loops back to asking name and bid.
+- Each person's name and bid are saved to a dictionary.
+- Once all participants have placed their bid, the program works out who has the highest bid and prints it.
+"""
+"""
+Demo from AppBrewery:
+https://appbrewery.github.io/python-day9-demo/
+"""
+
 from art import logo
 import os
 import subprocess
---
+++ Day-016-Coffee-Machine-in-OOP/main.py
@@ -0,0 +1,28 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+def prompt_order():
+    menu = Menu()
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+
+    order = input(f"What would you like? ({menu.get_items()}): ").lower()
+    if order not in menu:
+        if order == "report":
+            coffee_maker.report()
+            money_machine.report()
+        elif order == "off":
+            exit()
+        else:
+            print("I'm sorry, you didn't enter a valid order.")
+    else:
+        menu_item = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(menu_item.ingredients):
+            if money_machine.make_payment(menu_item.cost):
+                coffee_maker.make_coffee(menu_item)
+
+
+while True:
+    prompt_order()
